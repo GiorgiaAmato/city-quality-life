80f1719589ef0c6fc00ea80fdecc7ca2e4d4a7ea
Giorgia Amato <78232397+GiorgiaAmato@users.noreply.github.com>
2022-01-28 17:56:13 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,56 +1,41 @@
 let cityInput = document.getElementById("city-input");
 let btnSearch = document.getElementById('btnSearch');
-let cityName = document.getElementsByClassName("city_name");
-let cityScore = document.getElementsByClassName("city_score");
-let categoryName = document.getElementsByClassName("category_name");
+let cityName = document.getElementsByClassName("city_name")[0];
+let cityScore = document.getElementsByClassName("city_score")[0];
+let cityDescription = document.getElementsByClassName("city_description")[0];
+let categoryName = document.getElementsByClassName("category_name")[0];
 let categoryScore = document.getElementsByClassName("category_score")[0];
 
-function info(result) {
-    cityName.innerText = result;
-
-    if (result.status == 'ok') {
-        cityScore.innerHTML = result.teleport_city_score + ` - Good`;
-        cityScore.style.backgroundColor = `#009966`;
-        cityScore.style.color = 'white';
-        healthImplications.innerHTML = `Air quality is considered satisfactory, and air pollution poses little or no risk.`;
-        cautionaryStatement.innerHTML = "";
-
-    } else if (result.status == 'ok' && result.data.aqi > 51 && result.data.aqi <= 100) {
-        cityScore.innerHTML = result.data.aqi + ` - Moderate`;
-        cityScore.style.backgroundColor = `#ffde33`;
-        cityScore.style.color = 'white';
-        healthImplications.innerHTML = "";
-        cautionaryStatement.innerHTML = `<strong> Cautionary Statement</strong>: Active children and adults, and people with respiratory disease, such as asthma,
-                                        should limit prolonged outdoor exertion.`
-
-    } 
-}
-
 async function checkQuality() {
-    cityName.innerHTML = `&nbsp;`;
     let search = cityInput.value;
-    await fetch('https://api.teleport.org/api/urban_areas/slug:los-angeles/scores/')
+    await fetch(`https://api.teleport.org/api/urban_areas/slug:${search}/`)
         .then(function (response) {
-            console.log(response.json());
+            //console.log (response.json());
+            return response.json();     
         })
-        .then(info)
+        .then(function (result) {
+            cityName.innerHTML = result.full_name;
+        })
 
+    await fetch(`https://api.teleport.org/api/urban_areas/slug:${search}/scores/`)
+        .then(function (response) {
+            //console.log (response.json());
+            return response.json();     
+        })
+        .then(function (result) {
+            cityScore.innerHTML = "City Score: " + Math.floor(result.teleport_city_score);
+            cityDescription.innerHTML = result.summary;
+            console.log (result.categories[0].name);
+        })
         .catch(
             function (response) {
                 if (response.status = "error") {
-                    cityScore.innerHTML = `Not Found`;
-                    cityScore.style.backgroundColor = `red`;
-                    cityScore.style.color = 'white';
-                    healthImplications.innerHTML = `City not found, please enter a valid name.`;
-                    cautionaryStatement.innerHTML = "";
-                } else {
-                    cityScore.innerHTML = `Error!`;
-                    cityScore.style.backgroundColor = `red`;
-                    healthImplications.innerHTML = `Somenthig went wrong`;
-                    cautionaryStatement.innerHTML = "";
+                    cityName.innerHTML = "Error"
+                    cityScore.innerHTML = "Urban area not found";
                 }
             }
         )
+  
 };
 
 btnSearch.onclick = checkQuality;
@@ -58,7 +43,7 @@ btnSearch.onclick = checkQuality;
 // Tasto invio
 document.addEventListener('keydown', function (event) {
     if (event.code == 'Enter') {
-        document.getElementById("city").blur();
+        document.getElementById("cityInput").blur();
         checkQuality();
     }
 });
