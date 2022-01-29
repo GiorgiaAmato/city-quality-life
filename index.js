91079d7885bdb16c0ddcd8cfd4b24a3adfb8256f
Giorgia Amato <78232397+GiorgiaAmato@users.noreply.github.com>
2022-01-29 12:57:52 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ let btnSearch = document.getElementById('btnSearch');
 let cityName = document.getElementsByClassName("city_name")[0];
 let cityScore = document.getElementsByClassName("city_score")[0];
 let cityDescription = document.getElementsByClassName("city_description")[0];
-let categoryName = document.getElementsByClassName("category_name")[0];
+let cat1 = document.getElementsByClassName ('cat-1') [0];
+let cat2 = document.getElementsByClassName ('cat-2') [0];
 let categoryScore = document.getElementsByClassName("category_score")[0];
+
+
+
 
 async function checkQuality() {
     let search = cityInput.value;
@@ -25,16 +29,26 @@ async function checkQuality() {
         .then(function (result) {
             cityScore.innerHTML = "City Score: " + Math.floor(result.teleport_city_score);
             cityDescription.innerHTML = result.summary;
-            console.log (result.categories[0].name);
+            /*cat1.innerHTML = result.categories[0].name;
+            cat2.innerHTML = result.categories[1].name;*/
+
+            //crea nuovo li e assegnarli classe task
+            let li = document.createElement('li');
+            li.className = 'category-li';
+            li.appendChild(document.createTextNode(result.categories[0].name.value));
+            //inserire li dentre ul
+            let categoryList = document.getElementById ('#category-list')[0];
+            categoryList.appendChild(li);
         })
-        .catch(
+
+        /*.catch(
             function (response) {
                 if (response.status = "error") {
                     cityName.innerHTML = "Error"
                     cityScore.innerHTML = "Urban area not found";
                 }
             }
-        )
+        )*/
   
 };
 
